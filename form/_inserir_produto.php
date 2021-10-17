12b30e8247567f0e04d5db3e1891bf3cbac3535e
@Silvio47 <vitor989santos@gmail.com>
2021-10-17 17:09:16 -0300

    first commit

---
+++ form/_inserir_produto.php
@@ -0,0 +1,40 @@
+<?php
+    include 'conexao.php';
+
+    $nroproduto = $_POST['nroproduto']; 
+    $nomeproduto = $_POST['nomeproduto'];
+    $categoria = $_POST['categoria'];
+    $quantidade = $_POST['quantidade'];
+    $fornecedor = $_POST['fornecedor']; 
+
+   
+
+
+   
+    $sql ="INSERT INTO `estoque`(`nroproduto`, `nomeproduto`, `categoria`, `quantidade`, `fornecedor`) VALUES ($nroproduto,'$nomeproduto','$categoria',$quantidade,'$fornecedor')";
+
+
+    $inserir = mysqli_query($conexao, $sql);
+
+?>
+
+    <link rel = "preconnect" href = "https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/style.css">
+
+    <div class="container" style="width: 500px;margin-top: 20px;">
+        <center>
+            <h4>Produto Adicionado com sucesso!</h4>
+        </center>
+        
+        <div style="padding-top: 20px">
+            <center>
+               <a href="../index.php" role="button" class="btn btn=sm btn-primary">Cadastrar novo Ítem</a> 
+            </center>
+            
+        </div>
+        
+    </div>
---
+++ form/conexao.php
@@ -0,0 +1,8 @@
+<?php
+
+    $servername = 'localhost';
+    $database = 'bd_crup_procedural';
+    $username = 'root';
+    $password = '';
+
+    $conexao = mysqli_connect($servername, $username, $password, $database);
